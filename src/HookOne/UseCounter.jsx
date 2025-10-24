6acb948d063221a16591518a30365e88afa3a99f
K0UND1NYA <shrinidhiskoundinyassk@gmail.com>
2025-10-24 16:07:37 +0530

    24-10-25

--- src/HookOne/UseCounter.jsx
+++ src/HookOne/UseCounter.jsx
@@ -3,8 +3,8 @@ import {useState} from 'react'
 //custom Hook to manage counter logic 
 function UseCounter(initialValue=0) {
     const [count,setCount]=useState(initialValue);
-    const increment=()=>setCount(initialValue+1);
-    const decrement=()=>setCount(initialValue-1);
+    const increment=()=>setCount((prevCount)=>prevCount+1);
+    const decrement=()=>setCount((prevCount)=>prevCount-1);
     const reset=()=>setCount(initialValue);
     //Return anthing you want to share 
     return {count,increment,decrement,reset};
